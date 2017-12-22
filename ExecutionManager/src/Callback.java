da2a9eeb769d0c820d62afe5ba7afdd84fe2333a
segasp5 <segasp5@list.ru>
2017-12-22 21:24:53 +0300

    Stream and ExecManager remove

--- ExecutionManager/src/Callback.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by user6 on 15.12.2017.
- */
-public class Callback implements Runnable {
-    @Override
-    public void run() {
-        System.out.println("Callback");
-    }
-}
--- ExecutionManager/src/ExecutionManager.java
+++
@@ -1,6 +0,0 @@
-/**
- * Created by user6 on 15.12.2017.
- */
-public interface ExecutionManager {
-    Context execute(Runnable callback, Runnable... tasks);
-}
